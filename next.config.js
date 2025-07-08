2ce8096168c1aa4205c137008ad0e45a207badd8
Akash Jaiswal <realakashjaiswal@gmail.com>
2025-07-08 20:07:22 +0530

    add

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
+
 const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: '/',
   images: {
-    domains: isProd ? ['https://www.lhome.co.in'] : ['localhost'],
+    domains: isProd ? ['www.lhome.co.in'] : ['localhost'],
     unoptimized: isProd,
   },
-  // distDir: 'dist',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  output: isProd ? 'export' : undefined,
-  assetPrefix: isProd ? 'https://www.lhome.co.in' : undefined,
+  output: 'export',            // ✅ Keep this
   trailingSlash: true,
 };
 
 module.exports = nextConfig;
+
